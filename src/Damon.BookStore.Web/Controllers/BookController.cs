db99ba70e8dabb4c9b58e4d4b624d1ca3469f985
杨小东 <yangxiaodong@yodoo.net.cn>
2020-07-24 15:58:35 +0800

    列表样式调整

---
+++ src/Damon.BookStore.Web/Controllers/BookController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Damon.BookStore.Web.Controllers
+{
+    public class BookController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
