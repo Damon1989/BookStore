e21ef7bd452fd80334efac0e9cd169b9222ea13d
杨小东 <yangxiaodong@yodoo.net.cn>
2020-07-28 15:29:42 +0800

    vue element table pagination

--- src/Damon.BookStore.Web/spa/src/components/BookList.vue
+++ src/Damon.BookStore.Web/spa/src/components/BookList.vue
@@ -1,11 +1,67 @@
  <template>
   <div>
-    <el-button @click="search">查询</el-button>
-    <el-table :data="tableData" style="width: 100%">
-      <el-table-column prop="date" label="日期" width="180"></el-table-column>
-      <el-table-column prop="name" label="姓名" width="180"></el-table-column>
-      <el-table-column prop="address" label="地址"></el-table-column>
+    <el-row :gutter="20">
+      <el-col :span="1" :offset="18">
+        <el-button @click="search">查询</el-button>
+      </el-col>
+      <el-col :span="1" :offset="1">
+        <el-button @click="dialogNewFormVisible = true">new</el-button>
+      </el-col>
+    </el-row>
+    <el-table :data="tableData" border style="width: 100%">
+      <el-table-column prop="name" label="Name" width="180"></el-table-column>
+      <el-table-column prop="type" label="Type" width="180"></el-table-column>
+      <el-table-column prop="publishDate" label="Publish date"></el-table-column>
+      <el-table-column prop="price" label="Price"></el-table-column>
+      <el-table-column prop="creationTime" label="Creation Time"></el-table-column>
+      <el-table-column fixed="right" label="操作" width="100">
+        <template slot-scope="scope">
+          <el-button @click="viewSingle(scope.row)" type="text" size="small">查看</el-button>
+          <el-button type="text" size="small">编辑</el-button>
+        </template>
+      </el-table-column>
     </el-table>
+    <el-row :gutter="20">
+      <el-col :span="5" :offset="15">
+        <div class="block">
+          <el-pagination
+            layout="sizes,prev, pager, next"
+            :total="total"
+            :page-size="pageSize"
+            :page-sizes="[5, 10, 20, 50]"
+            :current-page="currentPage"
+            @prev-click="prevClick"
+            @next-click="nextClick"
+            @current-change="currentChange"
+            @size-change="sizeChange"
+          ></el-pagination>
+        </div>
+      </el-col>
+    </el-row>
+
+    <el-dialog title="图书" :visible.sync="dialogNewFormVisible">
+      <el-form :model="newform">
+        <el-form-item label="活动名称" :label-width="formLabelWidth">
+          <el-input v-model="newform.name" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="Publish Date" :label-width="formLabelWidth">
+          <el-date-picker type="date" v-model="newform.publishDate" autocomplete="off"></el-date-picker>
+        </el-form-item>
+        <el-form-item label="价格" :label-width="formLabelWidth">
+          <el-input v-model="newform.price" autocomplete="off"></el-input>
+        </el-form-item>
+        <!-- <el-form-item label="活动区域" :label-width="formLabelWidth">
+          <el-select v-model="form.region" placeholder="请选择活动区域">
+            <el-option label="区域一" value="shanghai"></el-option>
+            <el-option label="区域二" value="beijing"></el-option>
+          </el-select>
+        </el-form-item>-->
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogNewFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="newBook">确 定</el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 
@@ -13,36 +69,68 @@
 export default {
   data() {
     return {
-      tableData: [
-        {
-          date: "2016-05-02",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄",
-        },
-        {
-          date: "2016-05-04",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1517 弄",
-        },
-        {
-          date: "2016-05-01",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1519 弄",
-        },
-        {
-          date: "2016-05-03",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1516 弄",
-        },
-      ],
+      tableData: [],
+      total: 0,
+      hideonsiglepage: true,
+      currentPage: 1,
+      pageSize: 5,
+      dialogNewFormVisible: false,
+      formLabelWidth: "120px",
+      newform: {
+        name: "",
+        price: 0,
+      },
     };
   },
   methods: {
     search() {
-      this.$axios.get("/api/app/book").then((response) => {
-        console.log(response);
+      var skipCount = (this.currentPage - 1) * this.pageSize;
+      this.$axios
+        .get(
+          "/api/app/book?SkipCount=" +
+            skipCount +
+            "&MaxResultCount=" +
+            this.pageSize
+        )
+        .then((response) => {
+          this.tableData = response.data.items;
+          this.total = response.data.totalCount;
+        });
+    },
+    prevClick() {
+      if (this.currentPage > 1) {
+        this.currentPage = this.currentPage - 1;
+      }
+      this.search();
+    },
+    nextClick() {
+      console.log("next");
+      this.currentPage = this.currentPage + 1;
+      this.search();
+    },
+    currentChange(val) {
+      this.currentPage = val;
+      this.search();
+    },
+    sizeChange(val) {
+      this.pageSize = val;
+      this.currentPage = 1;
+      this.search();
+    },
+    newBook() {
+      var that = this;
+      this.$axios.post("/api/app/book", this.newform).then((response) => {
+        that.dialogNewFormVisible = false;
+        that.search();
       });
     },
+    viewSingle(row) {
+      console.log(row);
+      console.log(row.id);
+    },
+  },
+  mounted() {
+    this.search();
   },
 };
 </script>
