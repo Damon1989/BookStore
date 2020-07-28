96220e97bc825c11c975cebf93b01a06ef58394f
杨小东 <yangxiaodong@yodoo.net.cn>
2020-07-28 15:57:57 +0800

    vue list edit

--- src/Damon.BookStore.Web/spa/src/components/BookList.vue
+++ src/Damon.BookStore.Web/spa/src/components/BookList.vue
@@ -5,7 +5,7 @@
         <el-button @click="search">查询</el-button>
       </el-col>
       <el-col :span="1" :offset="1">
-        <el-button @click="dialogNewFormVisible = true">new</el-button>
+        <el-button @click="showNewBook">新增</el-button>
       </el-col>
     </el-row>
     <el-table :data="tableData" border style="width: 100%">
@@ -14,10 +14,11 @@
       <el-table-column prop="publishDate" label="Publish date"></el-table-column>
       <el-table-column prop="price" label="Price"></el-table-column>
       <el-table-column prop="creationTime" label="Creation Time"></el-table-column>
-      <el-table-column fixed="right" label="操作" width="100">
+      <el-table-column fixed="right" label="操作" width="200">
         <template slot-scope="scope">
-          <el-button @click="viewSingle(scope.row)" type="text" size="small">查看</el-button>
-          <el-button type="text" size="small">编辑</el-button>
+          <!-- <el-button @click="viewSingle(scope.row)" type="text" size="small">查看</el-button> -->
+          <el-button type="text" size="small" @click="editSingle(scope.row.id)">编辑</el-button>
+          <el-button @click.native.prevent="deleteSingle(scope.row.id)" type="text" size="small">移除</el-button>
         </template>
       </el-table-column>
     </el-table>
@@ -25,7 +26,7 @@
       <el-col :span="5" :offset="15">
         <div class="block">
           <el-pagination
-            layout="sizes,prev, pager, next"
+            layout="prev, pager, next,sizes,total"
             :total="total"
             :page-size="pageSize"
             :page-sizes="[5, 10, 20, 50]"
@@ -39,16 +40,16 @@
       </el-col>
     </el-row>
 
-    <el-dialog title="图书" :visible.sync="dialogNewFormVisible">
-      <el-form :model="newform">
+    <el-dialog title="图书" :visible.sync="dialogFormVisible">
+      <el-form :model="form">
         <el-form-item label="活动名称" :label-width="formLabelWidth">
-          <el-input v-model="newform.name" autocomplete="off"></el-input>
+          <el-input v-model="form.name" autocomplete="off"></el-input>
         </el-form-item>
-        <el-form-item label="Publish Date" :label-width="formLabelWidth">
-          <el-date-picker type="date" v-model="newform.publishDate" autocomplete="off"></el-date-picker>
+        <el-form-item label="发布日期" :label-width="formLabelWidth">
+          <el-date-picker type="date" v-model="form.publishDate" autocomplete="off"></el-date-picker>
         </el-form-item>
         <el-form-item label="价格" :label-width="formLabelWidth">
-          <el-input v-model="newform.price" autocomplete="off"></el-input>
+          <el-input v-model="form.price" autocomplete="off"></el-input>
         </el-form-item>
         <!-- <el-form-item label="活动区域" :label-width="formLabelWidth">
           <el-select v-model="form.region" placeholder="请选择活动区域">
@@ -58,7 +59,7 @@
         </el-form-item>-->
       </el-form>
       <div slot="footer" class="dialog-footer">
-        <el-button @click="dialogNewFormVisible = false">取 消</el-button>
+        <el-button @click="dialogFormVisible = false">取 消</el-button>
         <el-button type="primary" @click="newBook">确 定</el-button>
       </div>
     </el-dialog>
@@ -73,12 +74,15 @@ export default {
       total: 0,
       hideonsiglepage: true,
       currentPage: 1,
-      pageSize: 5,
-      dialogNewFormVisible: false,
+      pageSize: 10,
+      dialogFormVisible: false,
       formLabelWidth: "120px",
-      newform: {
+      new: true,
+      showOperate:false,
+      form: {
         name: "",
         price: 0,
+        id: "",
       },
     };
   },
@@ -117,16 +121,47 @@ export default {
       this.currentPage = 1;
       this.search();
     },
+    showNewBook() {
+      this.dialogFormVisible = true;
+      this.new = true;
+    },
     newBook() {
       var that = this;
-      this.$axios.post("/api/app/book", this.newform).then((response) => {
-        that.dialogNewFormVisible = false;
-        that.search();
+      if (this.new) {
+        this.$axios.post("/api/app/book", this.form).then((response) => {
+          that.dialogFormVisible = false;
+          that.search();
+        });
+      } else {
+        this.$axios
+          .put("/api/app/book/" + this.form.id, this.form)
+          .then((response) => {
+            that.dialogFormVisible = false;
+            that.search();
+          });
+      }
+    },
+    // viewSingle(row) {
+    //   console.log(row);
+    //   console.log(row.id);
+    // },
+    editSingle(id) {
+      this.new = false;
+      var that = this;
+      this.$axios.get("/api/app/book/" + id).then(function (result) {
+        console.log(result);
+        that.form = result.data;
+        that.dialogFormVisible = true;
       });
     },
-    viewSingle(row) {
-      console.log(row);
-      console.log(row.id);
+    deleteSingle(id) {
+      console.log(id);
+      var that = this;
+      this.$axios.delete("/api/app/book/" + id).then(function (result) {
+        console.log(result);
+        that.currentPage = 1;
+        that.search();
+      });
     },
   },
   mounted() {
