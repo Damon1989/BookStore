952530ee977a3bd6e8d98420a003694d9334addb
杨小东 <yangxiaodong@yodoo.net.cn>
2020-08-12 18:05:06 +0800

    侧边栏处理

---
+++ src/Damon.BookStore.Web/spa/.eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:vue/essential"
+    ],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 11,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "vue"
+    ],
+    "rules": {
+      // "semi": ["error", "always"],
+      // "quotes": ["error", "double"]
+    }
+};
