3136c9f363d6920243fa693426315678dfda510a
杨小东 <yangxiaodong@yodoo.net.cn>
2020-09-04 16:57:09 +0800

    leader init

--- src/Damon.BookStore.Web/spav1/src/views/leader/list.vue
+++ src/Damon.BookStore.Web/spav1/src/views/leader/list.vue
@@ -1,22 +1,256 @@
 <template>
- <div>list</div>
+  <div class="app-container">
+    <div class="filter-container">
+      <el-row>
+        <el-col :span="2" :offset="2" class="filtertext">
+            省：
+        </el-col>
+        <el-col :span="4" >
+        <el-select size="medium" v-model="listQuery.provice"  clearable  class="filter-item filtercontrol">
+              <el-option v-for="item in proviceList" :key="item" :label="item" :value="item" />
+        </el-select>
+        </el-col>
+         <el-col :span="2" class="filtertext">
+            市：
+        </el-col>
+        <el-col :span="4">
+        <el-select size="medium" v-model="listQuery.city"  clearable  class="filter-item filtercontrol">
+              <el-option v-for="item in cityList" :key="item" :label="item" :value="item" />
+        </el-select>
+        </el-col>
+        <el-col :span="2" class="filtertext">
+            区：
+        </el-col>
+        <el-col :span="4" >
+        <el-select size="medium" v-model="listQuery.area"  clearable  class="filter-item filtercontrol">
+              <el-option v-for="item in areaList" :key="item" :label="item" :value="item" />
+        </el-select>
+        </el-col>
+    </el-row>
+
+    <!-- <el-row>
+      <el-col :span="2" :offset="2" class="filtertext">
+            姓名：
+      </el-col>
+      <el-col :span="4" >
+        <el-input size="medium" v-model="listQuery.name" placeholder="请输入" class="filter-item filtercontrol"></el-input>
+      </el-col>
+       <el-col :span="2"  class="filtertext">
+            工号：
+      </el-col>
+      <el-col :span="4" >
+        <el-input size="medium" v-model="listQuery.jobNum" placeholder="请输入" class="filter-item filtercontrol"></el-input>
+      </el-col>
+       <el-col :span="2"  class="filtertext">
+            手机：
+      </el-col>
+      <el-col :span="4" >
+        <el-input size="medium" v-model="listQuery.phone" placeholder="请输入" class="filter-item filtercontrol"></el-input>
+      </el-col>
+    </el-row> -->
+    <el-row>
+      <el-col :span="2" :offset="2" class="filtertext">
+        <el-button size="medium" type="success" icon="el-icon-plus">新  建</el-button>
+      </el-col>
+      <el-col :span="4" :offset="12" class="filtertext">
+        <el-button size="medium" type="success">查  询</el-button>
+        <el-button size="medium">重  置</el-button>
+      </el-col>
+    </el-row>
+      <el-alert
+      title="该事业部总计SA 400 名,TL 52 名, BM 21 名, AM 6 名"
+      type="info"
+      description=""
+      show-icon :closable="false" class="filtertext">
+    </el-alert>
+
+<el-table
+      v-loading="listLoading"
+      :data="list"
+      border
+      fit
+      highlight-current-row
+      style="width: 100%;"
+      class="filtertext"
+    >
+      <el-table-column label="分公司"   align="center" width="100" >
+        <template slot-scope="{row}">
+          <span>{{ row.branchCompany }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="顾问编号"   align="center" width="120" >
+        <template slot-scope="{row}">
+          <span>{{ row.num }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="工号"  align="center" width="120" >
+        <template slot-scope="{row}">
+          <span>{{ row.jobNum }}</span>
+        </template>
+      </el-table-column>
+            <el-table-column label="姓名"  align="center" width="100" >
+        <template slot-scope="{row}">
+          <span>{{ row.name }}</span>
+        </template>
+      </el-table-column>
+       <el-table-column label="手机"  align="center" width="120" >
+        <template slot-scope="{row}">
+          <span>{{ row.phone }}</span>
+        </template>
+      </el-table-column>
+                  <el-table-column label="职位"  align="center" width="100" >
+        <template slot-scope="{row}">
+          <span>{{ row.position }}</span>
+        </template>
+      </el-table-column>
+                 <el-table-column label="等级"  align="center" width="100" >
+        <template slot-scope="{row}">
+          <span>{{ row.level }}</span>
+        </template>
+      </el-table-column>
+                   <el-table-column label="注册地址"  align="center" min-width="210">
+        <template slot-scope="{row}">
+          <span>{{ row.registerAddress }}</span>
+        </template>
+      </el-table-column>
+                 <el-table-column label="入职时间"  align="center" width="180" >
+        <template slot-scope="{row}">
+          <span>{{ row.entryDateTime }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="Actions" align="center" min-width="60" class-name="small-padding fixed-width">
+         <template slot-scope="scope">
+          <router-link :to="'/adviser/edit/'+scope.row.id">
+            <el-button type="primary" size="small" icon="el-icon-edit">
+              编辑
+            </el-button>
+          </router-link>
+        </template>
+      </el-table-column>
+    </el-table>
+       <d-pagination
+     v-show="total>0"
+     :total="total"
+      :page.sync="listQuery.page"
+      :limit.sync="listQuery.limit"
+       @pagination="getList" />
+    </div>
+    </div>
 </template>
 
 <script>
+
+const datalist = [{
+  id: 1,
+  branchCompany: 'CNTM',
+  num: '10402213',
+  jobNum: '2SH061.03',
+  name: '王小丽0',
+  phone: '13589784586',
+  position: 'TL',
+  level: '5星',
+  registerAddress: '浙,江省,杭州市,西湖区,古荡湾',
+  entryDateTime: '2016-09-21  08:50:08',
+}, {
+  id: 2,
+  branchCompany: 'CNTM',
+  num: '10402213',
+  jobNum: '2SH061.03',
+  name: '王小丽1',
+  phone: '13589784586',
+  position: 'TL',
+  level: '5星',
+  registerAddress: '浙,江省,杭州市,西湖区,古荡湾',
+  entryDateTime: '2016-09-21  08:50:08',
+}, {
+  id: 3,
+  branchCompany: 'CNTM',
+  num: '10402213',
+  jobNum: '2SH061.03',
+  name: '王小丽2',
+  phone: '13589784586',
+  position: 'TL',
+  level: '5星',
+  registerAddress: '浙,江省,杭州市,西湖区,古荡湾',
+  entryDateTime: '2016-09-21  08:50:08',
+}, {
+  id: 4,
+  branchCompany: 'CNTM',
+  num: '10402213',
+  jobNum: '2SH061.03',
+  name: '王小丽3',
+  phone: '13589784586',
+  position: 'TL',
+  level: '5星',
+  registerAddress: '浙,江省,杭州市,西湖区,古荡湾',
+  entryDateTime: '2016-09-21  08:50:08',
+}, {
+  id: 5,
+  branchCompany: 'CNTM',
+  num: '10402213',
+  jobNum: '2SH061.03',
+  name: '王小丽4',
+  phone: '13589784586',
+  position: 'TL',
+  level: '5星',
+  registerAddress: '浙,江省,杭州市,西湖区,古荡湾',
+  entryDateTime: '2016-09-21  08:50:08',
+}, {
+  id: 6,
+  branchCompany: 'CNTM',
+  num: '10402213',
+  jobNum: '2SH061.03',
+  name: '王小丽5',
+  phone: '13589784586',
+  position: 'TL',
+  level: '5星',
+  registerAddress: '浙,江省,杭州市,西湖区,古荡湾',
+  entryDateTime: '2016-09-21  08:50:08',
+}];
 const proviceList = ['上海市 ', '江苏省'];
 const cityList = ['上海市 ', '盐城市'];
 const areaList = ['浦东新区 ', '东台'];
 export default {
+  name: 'list',
   data() {
     return {
       proviceList,
       cityList,
       areaList,
+      list: null,
+      listLoading: false,
+      listQuery: {
+        provice: '',
+        city: '',
+        area: '',
+        name: '',
+        jobnum: '',
+        phone: '',
+        page: 1,
+        limit: 5,
+      },
+      total: 20,
     };
+  },
+  created() {
+    this.getList();
+  },
+  methods: {
+    getList() {
+      this.total = 400;
+      this.list = datalist.slice(0, this.listQuery.limit);
+    },
   },
 };
 </script>
 
 <style>
+.filtertext{
+  text-align: right;
+  margin-top: 6px;
+}
 
+.filtercontrol{
+  width: 220px;
+}
 </style>
